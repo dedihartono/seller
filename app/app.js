aee843fa780960752ba281c14ab1af35247134d8
Dedi Hartono <dedihartono1993@gmail.com>
2017-05-21 23:29:58 +0700

    belajar module, controller, directive

--- app/app.js
+++ app/app.js
@@ -1,8 +1,34 @@
+(function() {
+  var app = angular.module('appKatalog', []);
 
 
+  app.controller('katalogContoller', function() {
+    this.katalog = data;
+  });
 
-(function () {
-	
-	var app = angular.module('katalog', []);
-
+  var data = [{
+      nama: 'kalimaya',
+      harga: 70000,
+      keterangan: 'batu akik yang cukup diminati',
+      pembelian: true,
+    },
+    {
+      nama: 'merah delima',
+      harga: 170000,
+      keterangan: 'batu akik mahal',
+      pembelian: false,
+    },
+    {
+      nama: 'iris',
+      harga: 90000,
+      keterangan: 'batu akik',
+      pembelian: true,
+    },
+    {
+      nama: 'hijau',
+      harga: 270000,
+      keterangan: 'batu akik',
+      pembelian: false,
+    }
+  ];
 })();
