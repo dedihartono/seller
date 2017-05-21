5434b01f2140124995afc684da757b2c15d2436a
Dedi Hartono <dedihartono1993@gmail.com>
2017-05-21 23:42:11 +0700

    tambahan property

--- app/app.js
+++ app/app.js
@@ -11,24 +11,28 @@
       harga: 70000,
       keterangan: 'batu akik yang cukup diminati',
       pembelian: true,
+      barang_kosong :false,
     },
     {
       nama: 'merah delima',
       harga: 170000,
       keterangan: 'batu akik mahal',
       pembelian: false,
+      barang_kosong :true,
     },
     {
       nama: 'iris',
       harga: 90000,
       keterangan: 'batu akik',
       pembelian: true,
+      barang_kosong :false,
     },
     {
       nama: 'hijau',
       harga: 270000,
       keterangan: 'batu akik',
       pembelian: false,
+      barang_kosong :true,
     }
   ];
 })();
