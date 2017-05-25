e9d4b110fa696459810b5b12d872343bd3ef2a14
Dedi Hartono <dedihartono1993@gmail.com>
2017-05-25 23:11:26 +0700

    perbaikan tampilan

--- app/app.js
+++ app/app.js
@@ -1,38 +1,21 @@
 (function() {
-  var app = angular.module('appKatalog', []);
+  var app = angular.module('store', []);
 
+  var gem = [{
+    name: 'kalimaya',
+    price: 90000,
+    description: 'Loream ipsum',
+  }, {
+    name: 'kalimaya2',
+    price: 90000,
+    description: 'Loream ipsum',
+  }, {
+    name: 'kalimaya3',
+    price: 90000,
+    description: 'Loream ipsum',
+  }]
 
-  app.controller('katalogContoller', function() {
-    this.katalog = data;
-  });
-
-  var data = [{
-      nama: 'kalimaya',
-      harga: 70000,
-      keterangan: 'batu akik yang cukup diminati',
-      pembelian: true,
-      barang_kosong :false,
-    },
-    {
-      nama: 'merah delima',
-      harga: 170000,
-      keterangan: 'batu akik mahal',
-      pembelian: false,
-      barang_kosong :true,
-    },
-    {
-      nama: 'iris',
-      harga: 90000,
-      keterangan: 'batu akik',
-      pembelian: true,
-      barang_kosong :false,
-    },
-    {
-      nama: 'hijau',
-      harga: 270000,
-      keterangan: 'batu akik',
-      pembelian: false,
-      barang_kosong :true,
-    }
-  ];
+  app.controller('storeController', function() {
+    this.product = gem;
+  })
 })();
