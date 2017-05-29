57798675679aaa1691644f6020ec860a93843286
Dedi Hartono <dedihartono1993@gmail.com>
2017-05-29 22:58:44 +0700

    tambah image

--- app/app.js
+++ app/app.js
@@ -5,14 +5,32 @@
     name: 'kalimaya',
     price: 90000,
     description: 'Loream ipsum',
+    outStock: false,
+    canPurchase: true,
+    image: {
+      full: 'assets/images/0.jpg',
+      tumb: 'assets/images/0.jpg',
+    }
   }, {
     name: 'kalimaya2',
     price: 90000,
     description: 'Loream ipsum',
+    outStock: false,
+    canPurchase: true,
+    image: {
+      full: 'assets/images/1.jpg',
+      tumb: 'assets/images/1.jpg',
+    }
   }, {
     name: 'kalimaya3',
     price: 90000,
     description: 'Loream ipsum',
+    outStock: false,
+    canPurchase: false,
+    image: {
+      full: 'assets/images/2.jpg',
+      tumb: 'assets/images/2.jpg',
+    }
   }]
 
   app.controller('storeController', function() {
